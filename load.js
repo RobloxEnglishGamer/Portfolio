ad9192239f8a89826941a9d503b91aa995595f2a
RobloxEnglishGamer <robloxenglishgamer@gmail.com>
2024-02-29 10:15:08 +0100

    test

--- load.js
+++ load.js
@@ -1,9 +1,6 @@
 const TEXT = ["Loading","Loading.","Loading..","Loading..."];
 const INTERVAL_TIME = 250;
 
-const loadingDiv = document.querySelector(".loading");
-const loadingText = document.querySelector(".loading-text")
-
 let intervalID;
 
 function init(){
@@ -16,7 +13,9 @@ function init(){
 }
 
 function onEvent(){
-  clearInterval(intervalID);
+  //clearInterval(intervalID);
+  const loadingDiv = document.querySelector(".loading");
+  const loadingText = document.querySelector(".loading-text")
   loadingText.textContent = "Loaded!";
   loadingDiv.classList.add("loaded");
 }
